b177ceb42df5a5e1e1b897fbb19b39915c9a795f
Wald <118862072+Waldfe@users.noreply.github.com>
2023-02-28 13:26:46 -0300

    Padronize o codigo

--- escadaCode/main.c
+++ escadaCode/main.c
@@ -1,24 +1,29 @@
 #include<stdio.h>
 
-int main ( ) {
-    int n, i=0, vr[ 1 ], resposta;
+int main( ) {
+
+    int n;
+    int i=0;
+    int vr[ 1 ];
+    int resposta;
+
     vr[0] = 1;
     vr[1] = 2;
     vr[2] = 3;
 
-    printf("Digite o numero de degraus da escada: ");
+    printf( "Digite o numero de degraus da escada: " );
     scanf ( "%d", &n );
 
     if( n!= 1 || n != 2 || n!=3 ) {
-        for (i=3;i<n;i++) {
-            vr[ 0 ] = vr[ 1 ];
-            vr[ 1 ] = resposta;
+        for ( i=3; i<n; i++ ) {
+            vr[ 0 ]  = vr[ 1 ];
+            vr[ 1 ]  = resposta;
            resposta  = vr[ 0 ] + vr[ 1 ];
         }
     } else {
         resposta = n;
     }
 
-    printf("\n%d maneiras.", resposta);
+    printf( "\n%d maneiras.", resposta );
     
 }
